3dc90eb31d011a9737eb3cd069336ff6c279c4f6
Pamela Rodrigues <p.pml@hotmail.com>
2024-05-16 20:33:22 -0300

    Example of hooks and props

---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,22 @@
+﻿import { useState } from "react"
+
+function Contador() {
+
+  const [valor, setValor] = useState(0); //variavel de estado se chama valor, valor responsavel por guardar o numero do contator. setValor responsavel por alterar mudar o estado. igual 
+  
+  function handleClick(){
+    setValor(valor +1);
+  }
+  return (
+    <div>
+      <h2>Componente Contador</h2>
+
+      <p>O valor atual é: {valor}</p>
+
+      <button onClick={handleClick}>Adicionar 1</button>
+    </div>
+
+  )
+}
+
+export default Contador
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,19 @@
+﻿interface homeProps{
+  titulo: string;
+  texto: string;
+}
+
+
+function Home(props: homeProps) { //dentro do home e um objeto
+  return (
+    <> 
+    <div>Componente Home</div>
+    <div>
+      <h2>{props.titulo}</h2>  
+      <p>{props.texto}</p>
+    </div>
+    </>
+  )
+}
+
+export default Home
